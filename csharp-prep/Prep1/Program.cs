1cd2b77febf0e2eca897921c6c8584d4c90ca8d3
Cezar <lumiatti@gmail.com>
2024-03-04 06:42:10 -0300

    test3

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        string first_name, last_name;
+        
         Console.WriteLine("What is your FIRST name?");
-        string first_name = Console.ReadLine();
+        first_name = Console.ReadLine();
 
         Console.WriteLine("What is your LAST name?");
-        string last_name = Console.ReadLine();
+        
+        last_name = Console.ReadLine();
         
         Console.WriteLine($"My name is {last_name}, {first_name} {last_name}");
     }
