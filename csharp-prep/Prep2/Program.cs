80044e3721050e8f9e271d0dbbfa4909267ec5b3
Cezar <lumiatti@gmail.com>
2024-03-04 07:11:53 -0300

    test1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int my_grade;
+        string grade;
+
+        Console.WriteLine("What is you grade percentage?");
+        my_grade = int.Parse(Console.ReadLine());
+
+        if (my_grade >= 90)
+        {
+            grade = "A";
+        }        
+        else if (my_grade >= 80 && my_grade < 90)
+        {
+            grade = "B";
+        }           
+        else if (my_grade >= 70 && my_grade <80)
+        {
+            grade = "C";
+        }
+        else if (my_grade >= 60 && my_grade <70)
+        {
+            grade = "D";
+        }
+        else if (my_grade < 60)
+        {
+            grade = "F";
+        }        
+        
+        
+        Console.WriteLine($"Your grade is {grade}");
     }
 }
