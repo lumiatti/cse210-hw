0e3f8caaa3cbbdd5eb39eb96a10a6de020b95c38
Cezar <lumiatti@gmail.com>
2024-03-05 06:41:05 -0300

    final code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = -1;
+        List<int> my_list;
+        my_list = new List<int>();
+        int total = 0;
+        int largest = 0;
+        int average = 0;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished.");
+            number = int.Parse(Console.ReadLine());
+
+            if (number !=0)
+            {
+                my_list.Add(number);    
+            }
+        }
+      //  foreach (int n in my_list)
+      //  {
+      //      total = (total + n);            
+       // }
+        total = my_list.Sum();
+        average = total/my_list.Count();
+        largest = my_list.Max();
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
